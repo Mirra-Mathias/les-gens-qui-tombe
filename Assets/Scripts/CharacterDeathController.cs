977ee23267b9eb50c6a60c842dacc8929d380f7a
CLarroque <cecile.larroque6@gmail.com>
2022-07-01 14:15:59 +0200

    Delete music and add explosion

--- Assets/Scripts/CharacterDeathController.cs
+++ Assets/Scripts/CharacterDeathController.cs
@@ -9,19 +9,41 @@ public class CharacterDeathController : MonoBehaviour
     public ScoreManager scoreManager;
     
     private Rigidbody _rigidbody;
+
     private const int DeathYPosition = -40;
-    
+
+    public GameObject explosion;
+
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+
     private void FixedUpdate()
     {
         if (_rigidbody.position.y < DeathYPosition)
         {
-            SceneManager.LoadScene("GameOver");
-            scoreManager.Stop();
+            Instantiate(explosion, new Vector3(_rigidbody.position.x, _rigidbody.position.y, _rigidbody.position.z), Quaternion.identity);
+            // new WaitForSeconds(6);
+            StartCoroutine(ExecuteAfterTime(0.8f, () =>
+            {            
+                SceneManager.LoadScene("GameOver");
+                scoreManager.Stop();
+            }));
         }
     }
+
+    private bool isCoroutineExecuting = false;
+    IEnumerator ExecuteAfterTime(float time, Action task)
+    {
+        if (isCoroutineExecuting)
+            yield break;
+        isCoroutineExecuting = true;
+        yield return new WaitForSeconds(time);
+        task();
+        isCoroutineExecuting = false;
+    }
+
 }
