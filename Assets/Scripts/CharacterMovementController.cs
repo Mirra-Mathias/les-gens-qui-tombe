d667985628f1af9badc23c6b871566a20b527333
Jack-Studd <liam.milor@etu.iut-tlse3.fr>
2022-07-01 09:08:44 +0200

    Character fall

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -1,9 +1,18 @@
+using System;
 using UnityEngine;
 
 public class CharacterMovementController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float fallSpeed;
     [SerializeField] private float rotationSpeed;
+    private bool _isFalling = false;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
@@ -12,15 +21,24 @@ public class CharacterMovementController : MonoBehaviour
 
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         movementDirection.Normalize();
-        
-        GetComponent<Rigidbody>().velocity = movementDirection * speed * Time.deltaTime;
-        
-        
+
+        if (!_isFalling)
+        {
+            _rigidbody.velocity = movementDirection * speed * Time.deltaTime;
+        }
+
+
         if (movementDirection != Vector3.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             transform.rotation =
                 Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _isFalling = true;
+        _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, -fallSpeed, _rigidbody.velocity.z);
     }
 }
